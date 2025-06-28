9c2d49af674ece9a102ca23f4caa3f66910b8e2c
Aathithya Ananth <110254100+XDAathi@users.noreply.github.com>
2025-06-28 02:43:19 -0400

    Add Supabase authentication UI components and session management to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import './App.css';
 import supabase from './supabase-client';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [session, setSession] = useState(null);
 
-  return (<></>)
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />);
+  } else {
+    return (<div>Logged in!</div>);
+  }
 }
 
-export default App
+export default App;
